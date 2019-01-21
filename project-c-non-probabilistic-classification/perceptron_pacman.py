c1a5913d6f104305b7270f9559ed167dbf95fd2d
Jort van Gorkum <jortvangorkum@gmail.com>
2019-01-21 21:22:55 +0100

    Perceptron Pacman

--- project-c-non-probabilistic-classification/perceptron_pacman.py
+++ project-c-non-probabilistic-classification/perceptron_pacman.py
@@ -50,15 +50,4 @@ class PerceptronClassifierPacman(PerceptronClassifier):
             print "Starting iteration ", iteration, "..."
             for i in range(len(trainingData)):
                 "*** YOUR CODE HERE ***"
-                data = trainingData[i]
-                possibleActions = data[1]
-                correctAction = trainingLabels[i]
-
-                # Value: trainingData[0][0]['Stop']
-                for action in possibleActions:
-                    features = data[0][action]
-                    for feature, value in features.items():
-                        if action == correctAction:
-                            self.weights[feature] += value
-                        else:
-                            self.weights[feature] -= value
+                util.raiseNotDefined()
